e4c1b20531dca2cbabf628239fd3eb765b368473
sbatrow <79014783+sbatrow@users.noreply.github.com>
2021-03-23 14:06:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image-UploadBot ⚡️ ⚡️ Sithum Batrow 🇱🇰⚡️⚡️
+
+> A simplest telegram to telegra.ph image uploader bot
+
+## installation (easy way)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/sbatrow/Image-UploadBotSI/tree/master)
+
+Libraries used: => Pyrogram => Telegraph
+
+deploy [@sithum batrow](https://t.me/sibatrow)
---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import uuid
+import shutil
+import logging
+from pyrogram import Client, filters
+from creds import Credentials
+from telegraph import upload_file
+
+logging.basicConfig(level=logging.INFO)
+
+
+TGraph = Client(
+    "Image upload bot",
+    bot_token=Credentials.BOT_TOKEN,
+    api_id=Credentials.API_ID,
+    api_hash=Credentials.API_HASH,
+)
+
+
+@TGraph.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text(
+        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by ⚡ ⚡ Sithum Batrow 🇱🇰⚡⚡ @sibatrow",
+        True,
+    )
+
+
+@TGraph.on_message(filters.photo)
+async def getimage(client, message):
+    tmp = os.path.join("downloads", str(message.chat.id))
+    if not os.path.isdir(tmp):
+        os.makedirs(tmp)
+    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
+    dwn = await message.reply_text("Downloading...", True)
+    img_path = await client.download_media(message=message, file_name=img_path)
+    await dwn.edit_text("Uploading...")
+    try:
+        response = upload_file(img_path)
+    except Exception as error:
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    shutil.rmtree(tmp, ignore_errors=True)
+
+
+TGraph.run()
